3439ea2f7c68ca6efc865627b0597466442fb107
hani <haniyousfi@gmail.com>
2024-07-14 01:43:01 +0200

    Fix endpoints

--- portfolio_backend/web/api/message/schema.py
+++ portfolio_backend/web/api/message/schema.py
@@ -1,7 +1,8 @@
 from datetime import datetime
 from enum import Enum
+from uuid import UUID, uuid4
 
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 
 class MessageBy(str, Enum):
@@ -11,8 +12,8 @@ class MessageBy(str, Enum):
 
 
 class MessageDTO(BaseModel):
-    message_id: str
-    chat_id: str
+    message_id: UUID = Field(default_factory=uuid4)
+    chat_id: UUID
     message_text: str
     message_by: MessageBy
-    created_at: datetime
+    created_at: datetime = Field(default_factory=datetime.utcnow)
