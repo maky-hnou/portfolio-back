b21b542c82c13f6a99d365f68748959cec42f0ed
hani <haniyousfi@gmail.com>
2024-08-31 04:55:11 +0200

    Update ChatHandler configs

--- portfolio_backend/services/chat/config.py
+++ portfolio_backend/services/chat/config.py
@@ -1,13 +1,34 @@
-prompt = """You are a helpful assistant. Answer all questions, as short as possible, to the best of your ability
-based on the conversation history and the following context:\n\nContext: {context}."""
+# prompt = """
+# Your task is to engage in a conversation with users, answering their questions exclusively based on
+# the CONTEXT INFORMATION provided to you. You must strictly refrain from answering any off-topic questions or providing
+# information not contained within the given context. The current conversation and the provided context are your only
+# sources of knowledge for answering the questions. If you receive an off-topic or offensive message, reply with: "None".
+#
+# Context: {context}
+# """
+
+prompt = """
+Your task is to engage in conversation using only the provided CONTEXT. Answer questions strictly based on this 
+context and the current conversation. For off-topic, offensive messages or irrelevant questions, reply with: "None".
+
+Context: {context}
+"""
+
+general_context = """
+This conversation is about Hani, a Software/Data Engineer. You're having this conversation to answer people's questions
+about Hani's experience and professional life."""
 
 off_topic_response = """It is whether your message is off-topic or I do not have enough information to answer it.
 You have sent {off_topic_count} out of 3 off-topic messages. After the 3rd instance, this chat will be terminated.
 \nIf you think this is a mistake, please rephrase your question and ask it again."""
 
-ai_first_message = "Welcome to the chat, feel free to ask any question about Hani, and I'll do my best to answer."
+ai_first_message = """
+Welcome to the chat, feel free to ask any question about Hani's experience and work, and I'll do my best to answer."""
 
 limit_out_of_topic_message = "You reached 3 out of topic messages. This chat is terminated."
 limit_length_message = (
     "You reached the maximum length for a single conversation. This conversation has been terminated."
 )
+
+messages_limit = 30
+off_topic_count_limit = 3
