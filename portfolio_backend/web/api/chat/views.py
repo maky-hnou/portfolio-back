81c03d8038b6eeafc2f2ef5507bce045df432bcd
hani <haniyousfi@gmail.com>
2024-09-11 23:05:25 +0200

    Apply rate limiter

--- portfolio_backend/web/api/chat/views.py
+++ portfolio_backend/web/api/chat/views.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, HTTPException
+from fastapi import APIRouter, HTTPException, Request, Response
 from fastapi.param_functions import Depends
 from loguru import logger
 
@@ -9,6 +9,7 @@ from portfolio_backend.db.models.message_model import MessageModel
 from portfolio_backend.services.chat.config import ai_first_message, general_context, prompt
 from portfolio_backend.web.api.chat.schema import ChatDTO
 from portfolio_backend.web.api.message.schema import MessageBy, MessageDTO
+from portfolio_backend.web.rate_limiter import limiter
 
 router = APIRouter()
 
@@ -25,7 +26,14 @@ async def get_chat(chat_id: str, chat_dao: ChatDAO = Depends()) -> ChatDTO:
 
 
 @router.post("/chat", response_model=ChatDTO)
-async def create_chat(chat: ChatDTO, chat_dao: ChatDAO = Depends(), message_dao: MessageDAO = Depends()) -> ChatDTO:
+@limiter.limit("2 per 5 minute", error_message="Rate limit 2 per 5 minutes exceeded for creating a chat.")
+async def create_chat(
+    request: Request,  # noqa: ARG001
+    response: Response,  # noqa: ARG001
+    chat: ChatDTO,
+    chat_dao: ChatDAO = Depends(),
+    message_dao: MessageDAO = Depends(),
+) -> ChatDTO:
     logger.info(f"Creating new chat with data: {chat.dict()}")
     chat_model = ChatModel(**chat.dict())
     await chat_dao.add_single_on_conflict_do_nothing(model_instance=chat_model)
